7228fb087ac1c4df3833eaaf07aa0cd7a9919234
karen chan <karen@karen-chan.com>
2014-11-19 20:59:49 +0100

    Use latest openstax-accounts v0.12.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_requires = (
         'cnx-epub',
         'cnx-query-grammar',
         'colander',
-        'openstax-accounts>=0.11.0',
+        'openstax-accounts>=0.12.0',
         'PasteDeploy',
         'pyramid',
         'psycopg2>=2.5',
